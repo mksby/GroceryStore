a1c6b4fa42ff0876d61c7b70c8b77eb59c168d1d
max <maxim.troitsky@gmail.com>
2022-04-26 10:02:55 +0300

    bootstrap

---
+++ polls/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from polls.models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
